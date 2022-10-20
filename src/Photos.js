7e9dcd6d2f72b643f92338f1056f85316161cb10
olesia <infocooll.col@gmail.com>
2022-10-21 00:00:35 +0200

    changed link

--- src/Photos.js
+++ src/Photos.js
@@ -7,7 +7,7 @@ export default function Photos(props) {
           {props.photos.map(function (photo, index) {
             return (
               <div className="col-4" key={index}>
-                <a href={photo.src.original} target="_blak">
+                <a href={photo.src.original} target="_blak" rel="noreferrer">
                   <img src={photo.src.landscape} className="img-fluid" />
                 </a>
               </div>
